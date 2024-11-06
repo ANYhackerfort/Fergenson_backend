7c29fcbfbe8094c4a40c595620eb746d6214237c
ANYhackerfort <zhanglinghe610@gmail.com>
2024-11-05 23:39:22 -0800

    backend setted up

---
+++ CMakeFiles/WebSocketWithOpenCV.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WebSocketWithOpenCV.dir/src/openCV_s/process_image.cpp.o"
+  "CMakeFiles/WebSocketWithOpenCV.dir/src/openCV_s/process_image.cpp.o.d"
+  "CMakeFiles/WebSocketWithOpenCV.dir/src/websocket_s/websocket_server.cpp.o"
+  "CMakeFiles/WebSocketWithOpenCV.dir/src/websocket_s/websocket_server.cpp.o.d"
+  "WebSocketWithOpenCV"
+  "WebSocketWithOpenCV.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/WebSocketWithOpenCV.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/WebSocketWithOpenCV.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for WebSocketWithOpenCV.
+# This may be replaced when dependencies are built.
